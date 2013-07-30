a6e21a1cc04cffa1399991c5fe441123ff455110
hufei68 <hufei68@gmail.com>
2013-07-30 17:34:47 +0800

    Version1.0
    1. 制作了GUI界面
    2. 支持对excel数据读取
    3. shp字段
    4. 静态与动态GIF输出
    不足：
    1. 输出图像布局刷新功能欠缺
    2. 程序鲁棒性欠缺
    3. 还有些小bug要修改

--- FieldOperation.py
+++ FieldOperation.py
@@ -1,6 +1,7 @@
 #coding=utf-8
 import arcpy
 import csv
+import xlrd
 
 def Add_Field_Name( Filename_path, Fieldname_add, Fieldtype_add="DOUBLE", Fieldprecision=15, Fieldscale=5, fieldlength=30, fieldalias="", field_is_nullable="NULLABLE",fiedl_is_required="NON_REQUIRED"):
 	  #添加字段
@@ -10,15 +11,22 @@ def Delete_Field( Filename_path, Fieldname_delete ):
      #删除字段
      arcpy.DeleteField_management( Filename_path, Fieldname_delete)
 
-def Update_Field_Value( update_table_path, input_table_path, Join_field_name, Add_field_name ):
+def Update_Field_Value( update_table_path, input_table_path,index, Join_field_name, Add_field_name ):
 	  #添加字段值
     #打开要添加的数据源
-    reader = csv.reader(open('C:\Users\Hu Fei\Desktop\Data\data1.csv'),dialect = 'excel')
+    #reader = csv.reader( open(input_table_path),dialect = 'excel')
+    book = xlrd.open_workbook( input_table_path)
     #将数据.csv转入对应的列表中
+    data = book.sheet_by_index(index)
     Array_Old = []
     Array_new = []
-    for row in reader:
-      Array_Old.append(row)
+    for col in range(data.ncols):
+          values = []
+          for row in range(data.nrows):
+              values.append(data.cell(row,col).value)
+          print values
+          Array_Old.append( values)
+    print Array_Old
     row_num = len(Array_Old)
     col_num = len(Array_Old[0])
     print '%d ======%d' %(col_num,row_num)
@@ -36,16 +44,16 @@ def Update_Field_Value( update_table_path, input_table_path, Join_field_name, Ad
     #确定更新数据时连接的field和需要更新的field在列表中的位置
     Join_ID = 0
     Add_ID = 0
-    for x in xrange(0, len(Array_new)):
-        if Array_new[x][0] == Join_field_name:
+    for x in xrange(0, len(Array_new[0])):
+        if Array_new[0][x] == Join_field_name:
            Join_ID = x
-        if Array_new[x][0] == Add_field_name:
+        if Array_new[0][x] == Add_field_name:
            Add_ID = x
     #将需要使用的连接field和更新field存放在一个字典中
     Add_content = {}
-    for x in xrange( 1, len(Array_new[0]) ):
-       Add_content[str(Array_new[Join_ID][x])] = Array_new[Add_ID][x]
-    print Add_content
+    for x in xrange( 1, len(Array_new) ):
+       Add_content[str(Array_new[x][Join_ID])] = Array_new[x][Add_ID]
+    print "Update Value is " + str( Add_content )
 
     
    #根据连接field和更新field来更新update_table
@@ -53,10 +61,12 @@ def Update_Field_Value( update_table_path, input_table_path, Join_field_name, Ad
     row = cursor.next()
     i = 0
     while row:
-      ss = int(row.getValue(Join_field_name))
+      ss = float(row.getValue(Join_field_name))
       aa = str(ss)
+      print row.getValue(Join_field_name)
       row.setValue(Add_field_name, Add_content[aa])
       cursor.updateRow(row)                    #很重要，不更新数据写不进去
       row = cursor.next()
+    print "Update Success"
   
-#Update_Field_Value("C:\Program Files (x86)\ArcGIS\DeveloperKit10.0\Samples\data\BloomfieldTownship\SchoolTaxDistrict.shp", 'C:\Users\Administrator\Desktop\data1.csv', 'FID', 'TRY')
+#Update_Field_Value(r"C:\Users\Hu Fei\Desktop\Data\BloomfieldTownship\SchoolTaxDistrict.shp", r'C:\Users\Hu Fei\Desktop\Data\data1.xls',0, r'FID', r'Other')
--- GUI.py
+++ GUI.py
@@ -1,67 +1,138 @@
 #encoding:utf-8
 import Tkinter
- 
-#点击计算按钮后：
-def eBtnDo(event):
- val_1 = entry_1.get()
- val_2 = entry_2.get()
- val_3 = entry_3.get()
- bj = float(val_1)
- yll = float(val_2)
- yll = yll/1000
- yue = float(val_3)
- temp_1 = bj * yll * (1+yll)**yue
- temp_2 = ( (1+yll)**yue - 1)
- result =  temp_1 / temp_2 
- l_result_val.config(l_result_val,text=result)
+import FileDialog 
+import FieldOperation
+import ExportMap
+import xlrd
+import arcpy
+from PIL import Image, ImageSequence
+import sys, os
+from images2gif import writeGif
+
+#生成打开文件对话框
+def OpenArcFile( Event ):
+	global ArcFile
+	fd = FileDialog.LoadFileDialog( root )
+	ArcFile = fd.go()
+	Text_1.insert(1.0, ArcFile)
+	print ArcFile
+
+def OpenExcelFile( Event ):
+	global ExcelFile
+	fd = FileDialog.LoadFileDialog( root )
+	ExcelFile = fd.go()
+	Text_2.insert(1.0, ExcelFile)
+	print ExcelFile
+
+def OpenMxdFile( ):
+	global MxdFile
+	fd = FileDialog.LoadFileDialog( root )
+	MxdFile = fd.go()
+	Text_mxd.insert(1.0, MxdFile)
+	print MxdFile
+
+def Process( ):
+	"""
+	Fieldname = 'Other'
+	Unionname = 'FID'
+	ArcFile = r'C:\Users\Hu Fei\Desktop\Data\BloomfieldTownship\SchoolTaxDistrict.shp'
+	ExcelFile = r'C:\Users\Hu Fei\Desktop\Data\data1.xls'
+	MxdFile = r'E:\GitHub\Arcpy_GIF\Data\Test.mxd'
+	OutputFolder = r'C:\Users\Hu Fei\Desktop\Data'
+    """
+	Fieldname = entry_field.get()
+	Unionname = entry_union.get()
+	OutputFolder = entry_output.get()
+	
+	excel = xlrd.open_workbook( ExcelFile)
+	num_sheet = excel.nsheets
+	GIF_name = []
+	for ii in xrange(0,num_sheet):
+		  FieldOperation.Add_Field_Name( ArcFile, Fieldname, "DOUBLE")
+		  FieldOperation.Update_Field_Value(ArcFile, ExcelFile,ii , Unionname, Fieldname)
+		  out_put = OutputFolder + '\\' + str(ii) + '.gif'
+		  GIF_name.append(out_put)
+		  ExportMap.Change_GraduatedValuesSymbology( MxdFile, r"C:\Users\Hu Fei\Desktop\Data\Temp.lyr", Fieldname, out_put)
+	ExportMap.Make_Animated_Gif( GIF_name, OutputFolder+'\Animated_GIF.gif')
+
+
+
  
 #点击关闭按钮后： 
-def eBtnClose(event):
- root.destroy()
- 
- #所有界面的鼻祖
-root = Tkinter.Tk(className="属性表添加")
+def eBtnClose( ):
+    root.destroy()
 
-root.geometry("800x420+200+200") #在xy轴200-200的位置创建400*220的窗口
+
+ #定义文件名
+filename = ''
+ArcFile = ''
+ExcelFile = ''
+MxdFile = ''
+ #所有界面的鼻祖
+root = Tkinter.Tk(className="数据更新与动态GIF合成")
+
+root.geometry("600x440+200+200") #在xy轴200-200的位置创建400*220的窗口
  
 #开始写界面显示部份：
 #直到使用Tkinter.Frame布局之前，界面完全是一塌糊涂
 line_1 = Tkinter.Frame(root)
-l_1 = Tkinter.Label(line_1,text="贷款本金（元）：",width="20")
-entry_1 = Tkinter.Entry(line_1,width="10")
+l_1 = Tkinter.Label(line_1,text="Shape文件路径:",width="20")
+Text_1 = Tkinter.Text(line_1, height = 1, width = "35")
+btnArc = Tkinter.Button(line_1,text="打开",width="10")
+btnArc.bind('<Button-1>',OpenArcFile)#绑定按钮事件
 l_1.pack(side = "left",pady = 8)
-entry_1.pack(side = "left")
+Text_1.pack(side = "left")
 line_1.pack(side = "top",fill = "x")
+btnArc.pack(side="left",padx = 20)
  
 line_2 = Tkinter.Frame(root)
-l_2 = Tkinter.Label(line_2,text="月利率（‰）：",width="20")
-entry_2 = Tkinter.Entry(line_2,width="10")
+l_2 = Tkinter.Label(line_2,text="Excel文件路径",width="20")
+Text_2 = Tkinter.Text(line_2, height = 1, width = 35)
+btnExcel = Tkinter.Button(line_2,text="打开",width="10")
+btnExcel.bind('<Button-1>',OpenExcelFile)#绑定按钮事件
 l_2.pack(side = "left",pady = 8)
-entry_2.pack(side = "left")
+Text_2.pack(side = "left")
 line_2.pack(side = "top",fill = "x")
+btnExcel.pack(side="left",padx = 20)
+
+line_mxd = Tkinter.Frame(root)
+l_mxd = Tkinter.Label(line_mxd,text="Mxd文件路径:",width="20")
+Text_mxd = Tkinter.Text(line_mxd, height = 1, width = "35")
+btnMxd = Tkinter.Button(line_mxd,text="打开",width="10", command = OpenMxdFile)
+l_mxd.pack(side = "left",pady = 8)
+Text_mxd.pack(side = "left")
+line_mxd.pack(side = "top",fill = "x")
+btnMxd.pack(side="left",padx = 20)
  
 line_3 = Tkinter.Frame(root)
-l_3 = Tkinter.Label(line_3,text="还款月数（月）",width="20")
-entry_3 = Tkinter.Entry(line_3,width="10")
+l_3 = Tkinter.Label(line_3,text="输出文件路径",width="20")
+entry_output = Tkinter.Entry(line_3,width="40")
 l_3.pack(side = "left",pady = 8)
-entry_3.pack(side = "left")
+entry_output.pack(side = "left")
 line_3.pack(side = "top",fill = "x")
+
+line_union = Tkinter.Frame(root)
+l_union = Tkinter.Label(line_union,text="链接字段",width="20")
+entry_union = Tkinter.Entry(line_union,width="40")
+l_union.pack(side = "left",pady = 8)
+entry_union.pack(side = "left")
+line_union.pack(side = "top",fill = "x")
  
-line_result = Tkinter.Frame(root)
-l_result = Tkinter.Label(line_result,text="结果",width="20")
-l_result_val = Tkinter.Label(line_result,width="80")
-l_result.pack(side = "left",pady = 8)
-l_result_val.pack(side = "left")
-line_result.pack(side = "top",fill = "x")
- 
+line_field = Tkinter.Frame(root)
+l_field = Tkinter.Label(line_field,text="添加字段",width="20")
+entry_field = Tkinter.Entry(line_field,width="40")
+l_field.pack(side = "left",pady = 8)
+entry_field.pack(side = "left")
+line_field.pack(side = "top",fill = "x")
+
+
 line_btn = Tkinter.Frame(root)
-btnLogin = Tkinter.Button(line_btn,text="计算")
-btnLogin.bind('<Button-1>',eBtnDo)#绑定按钮事件
-btnLogin.pack(side="left")
-btnClose = Tkinter.Button(line_btn,text="关闭")
-btnClose.bind('<Button-1>',eBtnClose)#绑定按钮事件
-btnClose.pack(side="left")
-line_btn.pack(side = "top")
+btnLogin = Tkinter.Button(line_btn,text="更新字段并出图", command = Process, width = 20)
+btnClose = Tkinter.Button(line_btn,text="关闭", command = eBtnClose, width = 20)
+line_btn.pack(side = 'top', fill = "x", pady = 10)
+btnLogin.pack(side="left",padx = 50)
+btnClose.pack(side = 'left', padx = 30)
+
 #终于把界面显示部份写完了
- 
+
 root.mainloop()
